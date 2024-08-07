b52944b0b6b560b0695c173d76ea55959537e8ec
0xshreyk <shreyashkumar.me@gmail.com>
2024-08-07 19:13:28 +0530

    init

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, jsonify
+from flask_socketio import SocketIO, send
+from db import create_new_section, get_sections_and_quiz_data
+
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/create/section/<section_name>')
+def create_section_route(section_name):
+    boolx, status = create_new_section(section_name)
+    if (boolx == True):
+        return jsonify({'section_name' : section_name}), status
+    else:
+        return jsonify({'error' : status}), status
+@app.route('/get/sections')
+def get_all_sections_route():
+    return get_sections_and_quiz_data()
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=8080, host='0.0.0.0')
---
+++ db.py
@@ -0,0 +1,62 @@
+from pymongo import MongoClient
+
+client = MongoClient("localhost", 27017)
+
+
+db = client.testmaker
+
+
+# get all the quizes in a section
+def get_all_quizes_of_section(section_name):
+    """Get all quizes belonging to this section"""
+    coll = db.quizes
+    quizes = coll.find({"section": section_name})
+    return quizes
+
+# get all the questions in a quiz
+def get_all_questions_of_quiz(quiz_id):
+    """Gets all the questions of this quiz id"""
+    coll = db.questions
+    questions = coll.find({"quiz_id": quiz_id})
+    return questions
+
+def create_new_section(section_name):
+    """Makes a new section"""
+    coll = db.sections
+    if coll.find({'section_name': section_name}):
+        return False, 400
+    else:
+        coll.insert_one({'section_name': section_name})
+        return True, 200
+    
+
+def get_sections_and_quiz_data():
+    """Returns a list of sections and their internal data"""
+    sections_coll = db.sections
+    quizes_coll = db.quizes
+    
+    # Get all section names
+    sections = sections_coll.find()
+    
+    result = []
+    
+    for section in sections:
+        section_name = section.get('section_name')
+        
+        # Fetch quiz data for the section
+        quizes = quizes_coll.find({'section': section_name})
+        
+        quiz_names = []
+        total_duration = 0
+
+        for quiz in quizes:
+            quiz_names.append(quiz.get('quiz_name'))
+            total_duration += quiz.get('duration', 0)
+        
+        result.append({
+            'section_name': section_name,
+            'quiz_names': quiz_names,
+            'total_duration': total_duration
+        })
+    
+    return result
